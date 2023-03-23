3290f1ca8a0a27723ec89f3b94f54115cec8dab1
samgombiner <sam.gombiner@gmail.com>
2023-03-23 12:54:33 -0400

    self update and bug fix

--- src/main/java/com/example/viewcontrollertodolist/HelloApplication.java
+++ src/main/java/com/example/viewcontrollertodolist/HelloApplication.java
@@ -8,10 +8,15 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    HelloController helloController;
+
     @Override
+
     public void start(Stage stage) throws IOException {
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1440, 800);
+        helloController = fxmlLoader.getController();
         stage.setTitle("To-Do List App");
         stage.setScene(scene);
         stage.show();
@@ -20,4 +25,9 @@ public class HelloApplication extends Application {
     public static void main(String[] args) {
         launch();
     }
+
+    public void stop() throws Exception {
+        helloController.saveData();
+    }
+
 }
--- src/main/java/com/example/viewcontrollertodolist/HelloController.java
+++ src/main/java/com/example/viewcontrollertodolist/HelloController.java
@@ -1,14 +1,24 @@
 package com.example.viewcontrollertodolist;
 
+import javafx.application.Application;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
+import javafx.css.converter.StringConverter;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.CheckBoxListCell;
 import javafx.scene.control.cell.TextFieldListCell;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class HelloController {
     @FXML
@@ -21,20 +31,26 @@ public class HelloController {
     public ListView toDoListFri;
     public ListView toDoListSat;
     public ListView toDoListSun;
+    public Button save;
+    public Button load;
 
-    ObservableList<String> itemsMon;
-    ObservableList<String> itemsTue;
-    ObservableList<String> itemsWed;
-    ObservableList<String> itemsThu;
-    ObservableList<String> itemsFri;
-    ObservableList<String> itemsSat;
-    ObservableList<String> itemsSun;
+    ArrayList<ObservableList<String>> allObservableLists;
+    ArrayList<ListView<String>> allToDoLists;
+    public int i;
 
-    public void initialize() {
+    public void initialize() throws Exception {
+        restoreData();
+/*
+        for (ListView eachListView: allToDoLists) {
+            eachListView.setEditable(true);
+            eachListView.setCellFactory(TextFieldListCell.forListView());
+        }
 
-        toDoListMon.setEditable(true);
+
+ */
+     /*   toDoListMon.setEditable(true);
         toDoListMon.setCellFactory(TextFieldListCell.forListView());
-
+        //toDoListMon.setCellFactory(CheckBoxListCell.forListView(new Callback<String, ObservableValue<Boolean>>()
         toDoListTue.setEditable(true);
         toDoListTue.setCellFactory(TextFieldListCell.forListView());
 
@@ -52,7 +68,19 @@ public class HelloController {
 
         toDoListSun.setEditable(true);
         toDoListSun.setCellFactory(TextFieldListCell.forListView());
-
+*/
+        //allObservableLists = new ArrayList<>();
+        for (i = 0; i < 7; i = i + 1) {
+            allToDoLists.get(i).setEditable(true);
+            allToDoLists.get(i).setCellFactory(TextFieldListCell.forListView());
+            //allObservableLists.add(FXCollections.observableArrayList());
+            //allObservableLists.set(allToDoLists);
+            allToDoLists.get(i).setOnKeyPressed(event -> {
+                if (event.getCode() == KeyCode.BACK_SPACE) {
+                    allToDoLists.get(i).getItems().removeAll(allToDoLists.get(i).getSelectionModel().getSelectedItems());
+                }
+            });
+        /*
         itemsMon = FXCollections.observableArrayList();
         itemsTue = FXCollections.observableArrayList();
         itemsWed = FXCollections.observableArrayList();
@@ -60,14 +88,17 @@ public class HelloController {
         itemsFri = FXCollections.observableArrayList();
         itemsSat = FXCollections.observableArrayList();
         itemsSun = FXCollections.observableArrayList();
-
-        toDoListMon.setItems(itemsMon);
+         */
+/*
         toDoListTue.setItems(itemsTue);
         toDoListWed.setItems(itemsWed);
         toDoListThu.setItems(itemsThu);
         toDoListFri.setItems(itemsFri);
         toDoListSat.setItems(itemsSat);
         toDoListSun.setItems(itemsSun);
+ */
+        }
+
         comboBox.getItems().add("Monday");
         comboBox.getItems().add("Tuesday");
         comboBox.getItems().add("Wednesday");
@@ -75,15 +106,27 @@ public class HelloController {
         comboBox.getItems().add("Friday");
         comboBox.getItems().add("Saturday");
         comboBox.getItems().add("Sunday");
+        /*
+        comboBox.
+
+        ArrayList<ComboBox<String>> allComboBox = new ArrayList();
+        allComboBox.add(comboBox.getItems().add("Monday"));
+
+         */
+
+
+      //  restoreData();
 
     }
 
     //code for onAction
+    // public void listViewClick(){}
     public void onHelloButtonClick() {
-
-
-        String selectedIndex = comboBox.getSelectionModel().getSelectedItem().toString();
+        int selectedIndex = comboBox.getSelectionModel().getSelectedIndex();
         System.out.println(selectedIndex);
+        //allObservableLists.get(selectedIndex).add(textInput.getText());
+        allToDoLists.get(selectedIndex).getItems().add(textInput.getText());
+/*
         if (selectedIndex == "Monday") {
             String userTypeMon = textInput.getText();
             itemsMon.add(userTypeMon);
@@ -114,9 +157,10 @@ public class HelloController {
         }
 
 
+ */
         textInput.clear();
     }
-
+ /*
     public void deleteKeyMon( final KeyEvent keyEvent ) {
         final Object selectedItem;
         selectedItem = toDoListMon.getSelectionModel().getSelectedItem();
@@ -206,6 +250,48 @@ public class HelloController {
                 toDoListSun.getItems().remove(selectedItem);
                 //text.clear
             }
+        }
+    }
+
+     */
+
+    public void saveData() throws Exception {
+        FileOutputStream fileOut = new FileOutputStream("Save");
+        ObjectOutputStream out = new ObjectOutputStream(fileOut);
+        for (int i = 0; i < allToDoLists.size(); i++) {
+            ArrayList<String> TemporaryAllToDoLists = new ArrayList<>(allToDoLists.get(i).getItems());
+            out.writeObject(TemporaryAllToDoLists);
+        }
+
+
+        // Method for serialization of object
+        out.close();
+        fileOut.close();
+    }
+
+    public void restoreData()  {
+        allToDoLists = new ArrayList<>();
+        allToDoLists.add(toDoListMon);
+        allToDoLists.add(toDoListTue);
+        allToDoLists.add(toDoListWed);
+        allToDoLists.add(toDoListThu);
+        allToDoLists.add(toDoListFri);
+        allToDoLists.add(toDoListSat);
+        allToDoLists.add(toDoListSun);
+
+        try {
+            FileInputStream fileIn = new FileInputStream("Save");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            for (int i = 0; i < allToDoLists.size(); i++) {
+                ArrayList<String> temporaryAllToDoLists = (ArrayList<String>) in.readObject();
+                ObservableList temporaryObservableList = FXCollections.observableArrayList(temporaryAllToDoLists);
+                allToDoLists.get(i).setItems(temporaryObservableList);
+            }
+            // Method for deserialization of object
+            in.close();
+            fileIn.close();
+        } catch (Exception ex) {
+            System.out.println(ex);
         }
     }
 }
